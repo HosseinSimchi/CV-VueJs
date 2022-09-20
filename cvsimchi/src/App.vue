5f7f5e699639568c68be1755b36cf4c58ae8b8a3
= <=>
2022-09-20 15:24:10 +0430

    We devided project into some components to control better on it

--- cvsimchi/src/App.vue
+++ cvsimchi/src/App.vue
@@ -1,11 +1,31 @@
 <template>
 <div>
-  <h1>Salammmmmmmmmmmmmmmmmmmmmm</h1>
+  <headerSite />
+  <sliderSite />
+  <aboutSite />
+  <experienceSite />
+  <workSite />
+  <contactSite />
+  <footerSite />
 </div>
 </template>
 
 <script>
+import headerSite from './components/headerSite.vue' 
+import sliderSite from './components/sliderSite.vue' 
+import aboutSite from './components/aboutSite.vue' 
+import experienceSite from './components/experienceSite.vue' 
+import workSite from './components/workSite.vue' 
+import contactSite from './components/contactSite.vue' 
+import footerSite from './components/footerSite.vue' 
 export default {
   name: 'App',
+  components : {headerSite,
+   sliderSite,
+    aboutSite, 
+    experienceSite, 
+    workSite, 
+    contactSite,
+    footerSite}
 }
 </script>
